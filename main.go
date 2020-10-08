c7ab5c397e459daca10c3c17599717c040112a1b
rushins <ns.rushi@gmail.com>
2020-10-08 16:54:18 -0700

    files upload

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+)
+
+func scanPort(protocol, hostname string, port int) bool {
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
+
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
+
+func main() {
+	fmt.Println("Port Scanning")
+	open := scanPort("tcp", "localhost", 17477)
+	fmt.Printf("Port Open: %t\n", open)
+}
+
